224b4978b8b345a40b94fd525de74d37461fab60
kay <kay.krazzie@gmail.com>
2022-05-12 13:37:43 +1000

    - updated

--- client/src/app/components/PasswordList/index.jsx
+++ client/src/app/components/PasswordList/index.jsx
@@ -1,10 +1,43 @@
-import { Paper, Table, TableContainer, TableRow, TableHead, TableBody, TableCell, Typography } from '@mui/material';
-import React from 'react';
-import { useGetPasswordQuery } from '../../__generated/schema';
+import React, { useState } from 'react';
+import { Paper, Table, TableContainer, TableRow, TableHead, TableBody, TableCell, Typography, Alert, Snackbar } from '@mui/material';
+import { useGetPasswordQuery, useDeletePasswordMutation } from '../../__generated/schema';
 import { styles } from './index.styles';
+
+const defaultAlert = {
+  visibility: false,
+  type: '',
+  message: '',
+};
 
 const PasswordList = () => {
     const {data, loading} = useGetPasswordQuery();
+    const [alert, setAlert] = useState(defaultAlert);
+    const [deletePasswordMutation] = useDeletePasswordMutation();
+    const remove = async (p) => {
+        try {
+            await deletePasswordMutation({
+                variables: {
+                    domain: p.domain,
+                    email: p.email,
+                    pwd: p.pwd,
+                },
+                refetchQueries: [
+                    'getPassword'
+                ]
+            });
+            setAlert({
+                visibility: true,
+                type: 'success',
+                message: 'Successfully removed',
+            });
+        } catch(e) {
+            setAlert({
+                visibility: true,
+                type: 'error',
+                message: 'Failed to remove',
+            });
+        }
+    }
 
     return (
         <div style={styles.container}>
@@ -18,6 +51,7 @@ const PasswordList = () => {
                             <TableCell style={styles.bolder}>Domain</TableCell>
                             <TableCell style={styles.bolder}>Email</TableCell>
                             <TableCell style={styles.bolder}>Password</TableCell>
+                            <TableCell style={styles.bolder}>Actions</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
@@ -26,11 +60,21 @@ const PasswordList = () => {
                                 <TableCell>{p.domain}</TableCell>
                                 <TableCell>{p.email}</TableCell>
                                 <TableCell>{p.pwd}</TableCell>
+                                <TableCell style={styles.icon} onClick={() => remove(p)}>X</TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
                 </Table>
-                
+                <Snackbar 
+                    open={alert.visibility}
+                    autoHideDuration={3000}
+                    anchorOrigin={{vertical: 'bottom', horizontal: 'right'}}
+                    onClose={() => setAlert(defaultAlert)}
+                >
+                    <Alert variant="outlined" severity={alert.type}>
+                    {alert.message}
+                    </Alert>
+                </Snackbar>
             </TableContainer>     
         )}
         </div>
